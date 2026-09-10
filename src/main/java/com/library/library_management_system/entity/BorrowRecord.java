8f15bb4866d01225ac4f42866d871334dd0d7947
GurukiranaG <gurukirangurukiran8132@gmail.com>
2026-09-11 00:17:38 +0530

    Add borrow record entity and JPA relationships

---
+++ src/main/java/com/library/library_management_system/entity/BorrowRecord.java
@@ -0,0 +1,78 @@
+package com.library.library_management_system.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class BorrowRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate issueDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+    @ManyToOne
+    @JoinColumn(name="member_id",nullable=false)
+    private Member member;
+    @ManyToOne
+    @JoinColumn(name="book_id",nullable=false)
+    private Book book;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BorrowStatus status;
+    public BorrowRecord() {
+
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public BorrowStatus getStatus() {
+        return status;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public void setStatus(BorrowStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/library/library_management_system/repository/BorrowRecordRepository.java
@@ -0,0 +1,8 @@
+package com.library.library_management_system.repository;
+
+import com.library.library_management_system.entity.BorrowRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BorrowRecordRepository  extends JpaRepository<BorrowRecord,Long> {
+
+}
